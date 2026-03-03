c78465cdbc757a1b172f194df5e111f6c01b7dc5
Julio0o <julio180305@gmail.com>
2026-03-03 16:04:52 -0600

    ya casi termianamos, verificar pagina web y agregar link desde el repo

--- PAGINA-WEB/front/index.js
+++ PAGINA-WEB/front/index.js
@@ -1,10 +1,10 @@
 const productos = [
-    { id: 1, nombre: "Producto Premium 1", icono: "fa-book" },
-    { id: 2, nombre: "Producto Premium 2", icono: "fa-book" },
-    { id: 3, nombre: "Producto Premium 3", icono: "fa-book" },
-    { id: 4, nombre: "Producto Premium 4", icono: "fa-book" },
-    { id: 5, nombre: "Producto Premium 5", icono: "fa-book" },
-    { id: 6, nombre: "Producto Premium 6", icono: "fa-book" }
+    { id: 1, nombre: "Almendra", imagen: "../imagenes/almendra.jpg" },
+    { id: 2, nombre: "Atlas", imagen: "../imagenes/atlas.jpg" },
+    { id: 3, nombre: "Fahrenheit 451", imagen: "../imagenes/farenheit.jpg" },
+    { id: 4, nombre: "Haikyu!!", imagen: "../imagenes/haikyu.jpg" },
+    { id: 5, nombre: "La Tregua", imagen: "../imagenes/tregua.jpg" },
+    { id: 6, nombre: "Wigetta", imagen: "../imagenes/wigetta.jpg" }
 ];
 
 const carruselTrack = document.getElementById('carruselTrack');
@@ -14,9 +14,10 @@ function cargarProductos() {
         card.classList.add('producto-card');
         card.innerHTML = `
             <div class="producto-imagen">
-                <i class="fas ${prod.icono}"></i>
+                <img src="${prod.imagen}" alt="Portada ${prod.nombre}" style="width: 100%; height: 100%; object-fit: contain;">
             </div>
             <div class="producto-info">
+                <h3 style="font-size: 1.8rem; text-align: center; color: var(--dark); margin-bottom: 1.5rem;">${prod.nombre}</h3>
                 <div class="lineas-producto">
                     <div class="linea-prod"></div>
                     <div class="linea-prod"></div>
@@ -34,7 +35,7 @@ const btnDerecha = document.querySelector('.btn-derecha');
 const indicadores = document.querySelectorAll('.indicador');
 let index = 0;
 function actualizarCarrusel() {
-    const anchoTarjeta = document.querySelector('.producto-card').offsetWidth + 20; 
+    const anchoTarjeta = document.querySelector('.producto-card').offsetWidth + 20;
     carruselTrack.style.transform = `translateX(${-index * anchoTarjeta}px)`;
 
     indicadores.forEach((dot, i) => {
